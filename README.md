192a86978067b0bb5efa5a3a6128bf4f20f6be7a
Yikang Cao <caoyikang.andy@bytedance.com>
2021-06-03 20:39:44 +0800

    fix: leetcode

--- README.md
+++ README.md
@@ -18,3 +18,12 @@ S1是辅助栈，S2是存储栈
 ### 343. 整数拆分
 
 https://github.com/youngyangyang04/leetcode-master/blob/master/problems/0343.%E6%95%B4%E6%95%B0%E6%8B%86%E5%88%86.md
+
+### 96. 不同的二叉搜索树
+
+https://github.com/youngyangyang04/leetcode-master/blob/master/problems/0096.%E4%B8%8D%E5%90%8C%E7%9A%84%E4%BA%8C%E5%8F%89%E6%90%9C%E7%B4%A2%E6%A0%91.md
+
+### 416. 背包问题变体
+
+https://leetcode-cn.com/problems/partition-equal-subset-sum/solution/0-1-bei-bao-wen-ti-xiang-jie-zhen-dui-ben-ti-de-yo/
+
--- js/leetcode416.js
+++ js/leetcode416.js
@@ -25,16 +25,8 @@
  * @return {boolean}
  */
 var canPartition = function (nums) {
-  let sum = 0;
-  nums.map(num => {
-    sum += num;
-  });
-  if (sum % 2) {
-    return false;
-  }
-  sum /= 2;
   const m = nums.length;
-  const n = sum;
+  const n = nums[0].length;
   const dp = [];
   for (let i = 0; i < m; i++) {
     const row = [];
@@ -48,17 +40,6 @@ var canPartition = function (nums) {
     }
     dp.push(row);
   }
-
-  for (let i = 1; i < m; i++) {
-    for (let j = 1; j <= n; j++) {
-      const lastWeight = j - nums[i];
-      if (nums[i] > j) {
-        dp[i][j] = dp[i - 1][j];
-      }
-      else {
-        dp[i][j] = dp[i - 1][j - nums[i]] || dp[i - 1][j];
-      }
-    }
-  }
-  return dp[m - 1][n];
+  console.table(dp);
 };
+canPartition([1,5,11,5]);
